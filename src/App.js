adf854f72d4f83b3212c7d6f7cd6cfc171ba9ad7
mnsuccess <ngudiamanasse@gmail.com>
2022-01-12 16:38:28 +0200

    [NEW] setup Tailwindcss && Styled Components using Twin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
+import tw, { styled } from "twin.macro";
+
+export const Container = styled.div`
+  ${tw`
+        mx-10 my-10 min-h-screen  grid gap-10 sm:grid-cols-1 md:grid-cols-2 bg-white
+        relative md:fixed  inset-0 mb-10 p-32
+    `}
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <h1>Hello World Starward fun</h1>
-    </div>
+    </Container>
   );
 }
 
