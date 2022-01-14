402096167626313bf9f60b671f06479ce3201207
mnsuccess <ngudiamanasse@gmail.com>
2022-01-14 11:44:36 +0200

    [FIX] fix scrolling on the overview page

--- src/components/pages/Overview.styles.js
+++ src/components/pages/Overview.styles.js
@@ -2,8 +2,8 @@ import tw, { styled } from "twin.macro";
 
 export const Container = styled.div`
   ${tw`
-        mx-10 my-10 min-h-screen  grid gap-10 sm:grid-cols-1 md:grid-cols-2 bg-white
-        relative md:fixed  inset-0 mb-11 
+        mx-10 my-10  grid gap-10 sm:grid-cols-1 md:grid-cols-2 bg-white
+         md:fixed  inset-0 mb-11 overflow-y-auto
     `}
 `;
 
